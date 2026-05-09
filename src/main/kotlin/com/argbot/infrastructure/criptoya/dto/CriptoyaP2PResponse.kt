144106606eb628023e5bcc15ffa7a7acecc6bc3a
Fabio Giaquinta <fgiaquintaruiz@gmail.com>
2026-05-09 08:29:29 +0200

    chore: remove dead code CriptoyaP2PResponse

--- src/main/kotlin/com/argbot/infrastructure/criptoya/dto/CriptoyaP2PResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.argbot.infrastructure.criptoya.dto
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class CriptoyaP2PResponse(val ask: Double)
